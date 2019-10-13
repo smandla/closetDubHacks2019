e32f2f7513bdb66ba55d9610ab30a6ea5da56bc7
Kavya Mandla <mandla21@up.edu>
2019-10-12 18:43:32 -0700

    added functions

--- closetDict.py
+++ closetDict.py
@@ -1,25 +1,60 @@
 import json
-#createDict method
 
-#empty dictionary of closet items
+# createDict method
+
+# empty dictionary of closet items
 closetItems = {
 
 }
 
-#dictionary for colors
+# dictionary for colors
 color = {
-
+    # white to blue, red, black
+    "FFFFFF": ["0000FF", "FF0000", "000000"],
+    # red to yellow, white,
+    "FF0000": ["FFFF00", "FFFFFF"],
+    # brown to cream, green
+    "663300": ["FFFFCC", "006600"],
+    # orange to blue, white, black
+    "FFA500": ["0000FF", "FFFFFF", "000000"],
+    # green to black, brown
+    "006600": ["000000", "663300"]
 }
 
+# dictionary for items
 items = {
-
+    "2": "1",
+    "1": "2"
 }
+
 """adds to dictionary as items are added.
     key = color + value"""
-def addDict(closetItems,key, value):
-    closetItems[key] = value
-""""""
+
+
+def addDict(closetItems, key):
+    closetItems[key] = createsValue(key)
+
+
 def createsValue(key):
+    value = key.split(', ')
+    newColor = color.get(value[0])
+    newItem = items.get(value[1])
+    newValue = newColor[0] + ', ' + str(newItem)
+    return newValue
 
 
+def main():
+    print("Hello!")
+
+
+userInputColor = str(input("What color (hex) is your item?"))
+userInputItem = int(input("What item is it?"))
+input = userInputColor + ', ' + str(userInputItem)
+
+userInputColor1 = str(input("What color (hex) is your item?"))
+userInputItem1 = int(input("What item is it?"))
+input1 = userInputColor1 + ', ' + str(userInputItem1)
+addDict(closetItems, input1)
+result = addDict(closetItems)
+print(closetItems)
 
