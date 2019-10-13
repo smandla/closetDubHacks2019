4752de4512948eea5d8b3428a9dc766d5a344c50
Kavya Mandla <mandla21@up.edu>
2019-10-12 17:13:16 -0700

    initial commit pt.2

---
+++ closetDict.py
@@ -0,0 +1,25 @@
+import json
+#createDict method
+
+#empty dictionary of closet items
+closetItems = {
+
+}
+
+#dictionary for colors
+color = {
+
+}
+
+items = {
+
+}
+"""adds to dictionary as items are added.
+    key = color + value"""
+def addDict(closetItems,key, value):
+    closetItems[key] = value
+""""""
+def createsValue(key):
+
+
+
